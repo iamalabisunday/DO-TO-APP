65cc8ebef2751f34af0672a2fc78e863c7047789
Sunday Alabi <iamalabisunday@gmail.com>
2026-02-09 10:33:25 -0800

    initial commit

--- home.js
+++ home.js
@@ -13,3 +13,36 @@ window.addEventListener("DOMContentLoaded", () => {
   welcomeName.textContent = `${firstName}`;
   return true;
 });
+
+/////////////////////////////////////
+// Opena Task
+/////////////////////////////////////
+const taskBtn = document.getElementById("taskBtn");
+const newTaskToggle = document.getElementById("new-task-toggle");
+
+taskBtn.addEventListener("click", openTaskModal);
+
+function openTaskModal() {
+  if (newTaskToggle.classList.contains("modal-toggle")) {
+    newTaskToggle.classList.remove("modal-toggle");
+    return false;
+  }
+  newTaskToggle.classList.add("modal-toggle");
+  return true;
+}
+
+/////////////////////////////////////
+// close New Task Modal
+/////////////////////////////////////
+const closeNewTask = document.getElementById("close-new-task");
+
+newTaskToggle.addEventListener("click", closeTaskModal);
+
+function closeTaskModal() {
+  if (closeNewTask.classList.contains("new-task-modal")) {
+    closeNewTask.classList.remove("new-task-modal");
+    return false;
+  }
+  closeNewTask.classList.add("new-task-modal");
+  return true;
+}
